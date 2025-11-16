82c12dd55501f80d429576a3f66506957c2c0844
Julia Soares Pereira <161829566+juliasoares17@users.noreply.github.com>
2025-11-16 10:17:12 -0300

    Adicionando a parte do Strategy à União de Padrões

---
+++ Engenharia de Software 3/União de Padrões/strategy/TreinoEmagrecimento.java
@@ -0,0 +1,14 @@
+package uniao_de_padroes.strategy;
+
+import uniao_de_padroes.core.PlanoTreino;
+
+public class TreinoEmagrecimento implements TreinoStrategy {
+    @Override
+    public PlanoTreino gerarTreino(String nome, int idade, double peso, double altura) {
+        return new PlanoTreino(
+            nome,
+            "Treino para Emagrecimento",
+            "Circuitos rápidos, alta repetição, foco em gasto calórico."
+        );
+    }
+}
---
+++ Engenharia de Software 3/União de Padrões/strategy/TreinoHipermetrofia.java
@@ -0,0 +1,14 @@
+package uniao_de_padroes.strategy;
+
+import uniao_de_padroes.core.PlanoTreino;
+
+public class TreinoHipermetrofia implements TreinoStrategy {
+    @Override
+    public PlanoTreino gerarTreino(String nome, int idade, double peso, double altura) {
+        return new PlanoTreino(
+            nome,
+            "Treino de Hipertrofia",
+            "Séries pesadas, baixa repetição, foco em força."
+        );
+    }
+}
