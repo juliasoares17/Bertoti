32077bfa8329d33d45587851c2c6f5ee8fdb4bcf
Julia Soares Pereira <161829566+juliasoares17@users.noreply.github.com>
2025-11-15 22:43:35 -0300

    Adicionando os tipos de usuário do antipattern do Observer

---
+++ Engenharia de Software 3/Padrão Observer/Anti-pattern/usuarios/UsuarioComum.java
@@ -0,0 +1,15 @@
+package padrao_observer.antipattern.usuarios;
+
+public class UsuarioComum {
+
+    private String nome;
+
+    public UsuarioComum(String nome){
+        this.nome = nome;
+    };
+
+    public void notificarComum(String titulo) {
+        System.out.println("(Usuário Comum - " + nome + ") Nova notícia: " + titulo + "\n");
+    };
+
+}
---
+++ Engenharia de Software 3/Padrão Observer/Anti-pattern/usuarios/UsuarioPremium.java
@@ -0,0 +1,17 @@
+package padrao_observer.antipattern.usuarios;
+
+public class UsuarioPremium {
+    
+    private String nome;
+
+    public UsuarioPremium(String nome){
+        this.nome = nome;
+    };
+
+    public void notificarPremium(String titulo, String conteudo){
+        System.out.println("(Usuário Premium - " + nome + ") Nova notícia: " + titulo);
+        System.out.println("    Título: " + titulo);
+        System.out.println("    Conteúdo: " + conteudo + "\n");
+    };
+
+}
