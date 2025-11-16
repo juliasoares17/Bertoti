a7cb5b77472a3efc2425d9ce27e143222cde9018
Julia Soares Pereira <161829566+juliasoares17@users.noreply.github.com>
2025-11-16 12:46:06 -0300

    Update TreinoFlexibilidade.java

--- Engenharia de Software 3/União de Padrões/strategy/TreinoFlexibilidade.java
+++ Engenharia de Software 3/União de Padrões/strategy/TreinoFlexibilidade.java
@@ -1,3 +1,5 @@
+package uniao_de_padroes.strategy;
+
 import uniao_de_padroes.core.PlanoTreino;
 
 public class TreinoFlexibilidade implements TreinoStrategy {
@@ -10,4 +12,5 @@ public class TreinoFlexibilidade implements TreinoStrategy {
         );
     }
 }
+
 
