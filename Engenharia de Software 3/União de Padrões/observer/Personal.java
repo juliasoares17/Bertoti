16c77387881bd98c6861e61c4a1ad2d4171fb1c0
Julia Soares Pereira <161829566+juliasoares17@users.noreply.github.com>
2025-11-16 12:43:17 -0300

    Update Personal.java

--- Engenharia de Software 3/União de Padrões/observer/Personal.java
+++ Engenharia de Software 3/União de Padrões/observer/Personal.java
@@ -1,3 +1,5 @@
+package uniao_de_padroes.observer;
+
 import java.util.Collections;
 import java.util.Set;
 import uniao_de_padroes.core.PlanoTreino;
@@ -30,4 +32,5 @@ public class Personal implements Observer{
         return true;
     }
 }
+
 
