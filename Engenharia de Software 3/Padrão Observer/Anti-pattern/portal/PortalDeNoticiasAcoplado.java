e776a6cd4f4574448d2be053421ca227d11616f2
Julia Soares Pereira <161829566+juliasoares17@users.noreply.github.com>
2025-11-15 22:42:12 -0300

    Adicionando o arquivo do portal para o antipattern do Observer

---
+++ Engenharia de Software 3/Padrão Observer/Anti-pattern/portal/PortalDeNoticiasAcoplado.java
@@ -0,0 +1,32 @@
+package padrao_observer.antipattern.portal;
+
+import padrao_observer.antipattern.usuarios.UsuarioComum;
+import padrao_observer.antipattern.usuarios.UsuarioPremium;
+
+public class PortalDeNoticiasAcoplado {
+
+    private UsuarioComum usuarioComum;
+    private UsuarioPremium usuarioPremium;
+
+    public void setUsuarioComum(UsuarioComum u){
+        this.usuarioComum = u;
+    };
+
+    public void setUsuarioPremium(UsuarioPremium u){
+        this.usuarioPremium = u;
+    };
+
+    public void publicarNoticia(String titulo, String conteudo){
+        System.out.println("Nova notícia publicada: " + titulo + "\n");
+
+        if (usuarioComum != null){
+            usuarioComum.notificarComum(titulo);
+        };
+
+        if (usuarioPremium != null) {
+            usuarioPremium.notificarPremium(titulo, conteudo);
+        };
+
+    };
+    
+}
