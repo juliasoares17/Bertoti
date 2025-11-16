f69af05fcde1ed39fc323273b630406f2dd5cb61
Julia Soares Pereira <161829566+juliasoares17@users.noreply.github.com>
2025-11-16 12:16:10 -0300

    Update TreinoStrategy.java

--- Engenharia de Software 3/União de Padrões/strategy/TreinoStrategy.java
+++ Engenharia de Software 3/União de Padrões/strategy/TreinoStrategy.java
@@ -1,7 +1,6 @@
-package uniao_de_padroes.strategy;
-
 import uniao_de_padroes.core.PlanoTreino;
 
 public interface TreinoStrategy {
     PlanoTreino gerarTreino(String nomeAluno, int idade, double peso, double altura);
 }
+
