bf570b51c190c135c75a96925ed5ec67bc07a6f5
Julia Soares Pereira <161829566+juliasoares17@users.noreply.github.com>
2025-11-16 10:14:50 -0300

    Adicionando o Plano de Treino da união de padrões

---
+++ Engenharia de Software 3/União de Padrões/core/PlanoTreino.java
@@ -0,0 +1,25 @@
+package uniao_de_padroes.core;
+
+public class PlanoTreino {
+    private String nomeAluno;
+    private String tipo;
+    private String descricao;
+
+    public PlanoTreino(String nomeAluno, String tipo, String descricao) {
+        this.nomeAluno = nomeAluno;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
